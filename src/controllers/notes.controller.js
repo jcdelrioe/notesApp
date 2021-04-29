bb972a062b62d1aa2f30cb9b838f231c37dc5444
Juan Carlos Del Rio <jcdre@hotmail.com>
2021-04-29 12:28:56 -0500

    render all notes

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -1,16 +1,21 @@
 const notesCtrl = {};
 
+const Note = require("../models/Note");
+
 notesCtrl.renderNoteForm = (req, res) => {
 	res.render("notes/new-note");
 };
 
-notesCtrl.createNewNote = (req, res) => {
-	console.log(req.body);
+notesCtrl.createNewNote = async (req, res) => {
+	const { title, description } = req.body;
+	const newNote = new Note({ title, description });
+	await newNote.save();
 	res.send("New Note");
 };
 
-notesCtrl.renderNotes = (req, res) => {
-	res.send("Notes listed");
+notesCtrl.renderNotes = async (req, res) => {
+	const notes = await Note.find().lean();
+	res.render("notes/all-notes", { notes });
 };
 
 notesCtrl.renderEditForm = (req, res) => {
