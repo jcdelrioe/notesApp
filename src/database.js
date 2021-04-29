ebb0e38d4f6af343b5806530c506554502114a62
Juan Carlos Del Rio <jcdre@hotmail.com>
2021-04-28 19:27:17 -0500

    mongodb config

--- src/database.js
+++ src/database.js
@@ -1,1 +1,18 @@
+const mongoose = require("mongoose");
 
+// import config from "./config";
+
+const config = require("./config");
+
+const mongooseOptions = {
+	useUnifiedTopology: true,
+	useNewUrlParser: true,
+};
+
+mongoose
+	.connect(
+		`mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@${config.MONGO_HOST}/${config.MONGO_DATABASE}`,
+		mongooseOptions
+	)
+	.then((db) => console.log("DB is connected to: ", db.connection.name))
+	.catch((err) => console.log(err));
